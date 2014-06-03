31050c2f70f9b901b07e805db2a000a7e5e7444c
Xi Chen <chenx19@miamioh.edu>
2014-06-03 17:37:45 -0400

    replace tab with spaces

--- Algorithms and Data Structures/src/algorithms/sorting/QuickSort.java
+++ Algorithms and Data Structures/src/algorithms/sorting/QuickSort.java
@@ -17,74 +17,74 @@ package algorithms.sorting;
  */
 public final class QuickSort {
 
-	/**
-	 * Prevent instantiation
-	 */
-	private QuickSort() {
+    /**
+     * Prevent instantiation
+     */
+    private QuickSort() {
 
-	}
+    }
 
-	/**
-	 * Sort the specified array. The array must be comparable
-	 * 
-	 * @param a the array to be sorted
-	 */
-	public static <T extends Comparable<T>> void sort(T[] a) {
-		if (a == null || a.length == 0) {
-			return;
-		}
-		sort(a, 0, a.length - 1);
-	}
+    /**
+     * Sort the specified array. The array must be comparable
+     * 
+     * @param a the array to be sorted
+     */
+    public static <T extends Comparable<T>> void sort(T[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        sort(a, 0, a.length - 1);
+    }
 
-	/**
-	 * Sort the specified range array.
-	 * 
-	 * @param a the array to be sorted
-	 * @param low low index of the array, inclusive, to be sorted
-	 * @param high high index of the array, inclusive, to be sorted
-	 */
-	public static <T extends Comparable<T>> void sort(T[] a, int low,
-			int high) {
-		if (low >= high) { // Base case - only 1 element
-			return;
-		}
+    /**
+     * Sort the specified range array.
+     * 
+     * @param a the array to be sorted
+     * @param low low index of the array, inclusive, to be sorted
+     * @param high high index of the array, inclusive, to be sorted
+     */
+    public static <T extends Comparable<T>> void sort(T[] a, int low,
+            int high) {
+        if (low >= high) { // Base case - only 1 element
+            return;
+        }
 
-		int i = low - 1; // All items at or in the left of i must < pivot
-		int j = high; // All items at or in the right of j must > pivot
+        int i = low - 1; // All items at or in the left of i must < pivot
+        int j = high; // All items at or in the right of j must > pivot
 
-		int randomIdx = (int) ((high - low) * Math.random()) + low;
-		swap(a, randomIdx, high);
-		T pivot = a[high]; // pivot always choose the last index
+        int randomIdx = (int) ((high - low) * Math.random()) + low;
+        swap(a, randomIdx, high);
+        T pivot = a[high]; // pivot always choose the last index
 
-		// Find the pivot position and assign it to i
-		while (i < j) {
-			do { // Increment i as long as a[i] < pivot
-				i++;
-			} while (a[i].compareTo(pivot) < 0);
-			do { // Decrease j as long as a[j] > pivot
-				j--;
-			} while (a[j].compareTo(pivot) > 0 && j > i);
-			if (i < j) { // Swap only if i is left to j
-				swap(a, i, j);
-			}
-		}
+        // Find the pivot position and assign it to i
+        while (i < j) {
+            do { // Increment i as long as a[i] < pivot
+                i++;
+            } while (a[i].compareTo(pivot) < 0);
+            do { // Decrease j as long as a[j] > pivot
+                j--;
+            } while (a[j].compareTo(pivot) > 0 && j > i);
+            if (i < j) { // Swap only if i is left to j
+                swap(a, i, j);
+            }
+        }
 
-		swap(a, i, high); // Swap pivot the the right position
+        swap(a, i, high); // Swap pivot the the right position
 
-		sort(a, low, i - 1);
-		sort(a, i + 1, high);
-	}
+        sort(a, low, i - 1);
+        sort(a, i + 1, high);
+    }
 
-	/**
-	 * Swap two elements in an array based on the given two indices
-	 * 
-	 * @param idx1 the first index
-	 * @param idx2 the second index
-	 */
-	public static <T extends Comparable<T>> void swap(T[] a, int idx1, int idx2) {
-		T temp = a[idx1];
-		a[idx1] = a[idx2];
-		a[idx2] = temp;
-	}
+    /**
+     * Swap two elements in an array based on the given two indices
+     * 
+     * @param idx1 the first index
+     * @param idx2 the second index
+     */
+    public static <T extends Comparable<T>> void swap(T[] a, int idx1, int idx2) {
+        T temp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = temp;
+    }
 
 }
--- Algorithms and Data Structures/src/algorithms/sorting/Test.java
+++ Algorithms and Data Structures/src/algorithms/sorting/Test.java
@@ -4,79 +4,79 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Test {
-	public static final int NUM = 100000;
+    public static final int NUM = 100000;
 
-	// Worked!
-	public static void swap(int[] a, int[] b) {
-		int temp = a[0];
-		a[0] = b[0];
-		b[0] = temp;
-	}
+    // Worked!
+    public static void swap(int[] a, int[] b) {
+        int temp = a[0];
+        a[0] = b[0];
+        b[0] = temp;
+    }
 
-	/*
-	 * Won't work! Although Java manipulates objects 'by reference', Java pass
-	 * references to methods 'by value'; Also, Integer is immutable.
-	 * 
-	 * Refer to
-	 * http
-	 * ://www.javaworld.com/article/2077424/learn-java/does-java-pass-by-reference
-	 * -or-pass-by-value.html
-	 */
-	public static void swap(Integer a, Integer b) {
-		Integer temp = a;
-		a = b;
-		b = temp;
-	}
+    /*
+     * Won't work! Although Java manipulates objects 'by reference', Java pass
+     * references to methods 'by value'; Also, Integer is immutable.
+     * 
+     * Refer to
+     * http
+     * ://www.javaworld.com/article/2077424/learn-java/does-java-pass-by-reference
+     * -or-pass-by-value.html
+     */
+    public static void swap(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
 
-	// Verify the correctness of quickSort() implementation
-	public static void testQuickSort() {
-		Integer[] unsort1 = new Integer[NUM];
-		Integer[] unsort2 = new Integer[NUM];
-		for (int i = 0; i < NUM; i++) {
-			unsort1[i] = new Random().nextInt(NUM);
-			unsort2[i] = unsort1[i];
-		}
-		QuickSort.sort(unsort1);
-		Arrays.sort(unsort2);
+    // Verify the correctness of quickSort() implementation
+    public static void testQuickSort() {
+        Integer[] unsort1 = new Integer[NUM];
+        Integer[] unsort2 = new Integer[NUM];
+        for (int i = 0; i < NUM; i++) {
+            unsort1[i] = new Random().nextInt(NUM);
+            unsort2[i] = unsort1[i];
+        }
+        QuickSort.sort(unsort1);
+        Arrays.sort(unsort2);
 
-		for (int i = 0; i < NUM; i++) {
-			if (unsort1[i].intValue() != unsort2[i].intValue()) {
-				System.out.println(unsort1[i]);
-				System.out.println(unsort2[i]);
-				System.out.println("Sometihng is wrong for quick sort!!!");
-				System.out.println("i = " + i);
-				return;
-			}
-		}
-		System.out.println("Quick Sort implementation Passed!");
-	}
+        for (int i = 0; i < NUM; i++) {
+            if (unsort1[i].intValue() != unsort2[i].intValue()) {
+                System.out.println(unsort1[i]);
+                System.out.println(unsort2[i]);
+                System.out.println("Sometihng is wrong for quick sort!!!");
+                System.out.println("i = " + i);
+                return;
+            }
+        }
+        System.out.println("Quick Sort implementation Passed!");
+    }
 
-	// Verify the correctness of quickSort() implementation
-	public static void testQuickSort2() {
-		Double[] unsort1 = new Double[NUM];
-		Double[] unsort2 = new Double[NUM];
-		for (int i = 0; i < NUM; i++) {
-			unsort1[i] = NUM * new Random().nextDouble();
-			unsort2[i] = unsort1[i];
-		}
-		QuickSort.sort(unsort1);
-		Arrays.sort(unsort2);
+    // Verify the correctness of quickSort() implementation
+    public static void testQuickSort2() {
+        Double[] unsort1 = new Double[NUM];
+        Double[] unsort2 = new Double[NUM];
+        for (int i = 0; i < NUM; i++) {
+            unsort1[i] = NUM * new Random().nextDouble();
+            unsort2[i] = unsort1[i];
+        }
+        QuickSort.sort(unsort1);
+        Arrays.sort(unsort2);
 
-		for (int i = 0; i < NUM; i++) {
-			if ((unsort1[i].doubleValue() - unsort2[i].doubleValue()) > 10e-6) {
-				System.out.println(unsort1[i]);
-				System.out.println(unsort2[i]);
-				System.out.println("Sometihng is wrong for quick sort!!!");
-				System.out.println("i = " + i);
-				return;
-			}
-		}
-		System.out.println("Quick Sort implementation Passed!");
-	}
+        for (int i = 0; i < NUM; i++) {
+            if ((unsort1[i].doubleValue() - unsort2[i].doubleValue()) > 10e-6) {
+                System.out.println(unsort1[i]);
+                System.out.println(unsort2[i]);
+                System.out.println("Sometihng is wrong for quick sort!!!");
+                System.out.println("i = " + i);
+                return;
+            }
+        }
+        System.out.println("Quick Sort implementation Passed!");
+    }
 
-	public static void main(String[] args) {
-		testQuickSort();
-		testQuickSort2();
-	}
+    public static void main(String[] args) {
+        testQuickSort();
+        testQuickSort2();
+    }
 
 }
