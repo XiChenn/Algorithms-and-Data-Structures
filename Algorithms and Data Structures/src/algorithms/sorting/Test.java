8aaedd7d4e83ec89681dbf323f941dd78d59c620
Xi Chen <chenx19@miamioh.edu>
2014-06-03 16:11:50 -0400

    The previous Quick Sort implementation is correct, but the test method was implemented incorrectly

--- Algorithms and Data Structures/src/algorithms/sorting/Test.java
+++ Algorithms and Data Structures/src/algorithms/sorting/Test.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Test {
-	public static final int NUM = 10000;
+	public static final int NUM = 100000;
 
 	// Worked!
 	public static void swap(int[] a, int[] b) {
@@ -37,16 +37,43 @@ public class Test {
 		Arrays.sort(unsort2);
 
 		for (int i = 0; i < NUM; i++) {
-			if (unsort1[i] != unsort2[i]) {
+			if (unsort1[i].intValue() != unsort2[i].intValue()) {
+				System.out.println(unsort1[i]);
+				System.out.println(unsort2[i]);
 				System.out.println("Wrong!!!");
+				System.out.println("i = " + i);
 				return;
 			}
 		}
-		System.out.println("Correct!");
+		System.out.println("Passed!");
+	}
+
+	// Verify the correctness of quickSort() implementation
+	public static void testQuickSort2() {
+		Double[] unsort1 = new Double[NUM];
+		Double[] unsort2 = new Double[NUM];
+		for (int i = 0; i < NUM; i++) {
+			unsort1[i] = NUM * new Random().nextDouble();
+			unsort2[i] = unsort1[i];
+		}
+		QuickSort.quickSort(unsort1);
+		Arrays.sort(unsort2);
+
+		for (int i = 0; i < NUM; i++) {
+			if ((unsort1[i].doubleValue() - unsort2[i].doubleValue()) > 10e-6) {
+				System.out.println(unsort1[i]);
+				System.out.println(unsort2[i]);
+				System.out.println("Wrong!!!");
+				System.out.println("i = " + i);
+				return;
+			}
+		}
+		System.out.println("Passed!");
 	}
 
 	public static void main(String[] args) {
 		testQuickSort();
+		testQuickSort2();
 	}
 
 }
